adf3404531ffff2a81cd2d281c0d83ce4a10b9d1
Hyebin (Helia) <hyebin218@naver.com>
2023-06-08 11:46:34 +0900

    Update SceneKitTest

--- SceneKitTest/SceneKitTest/PlantView.swift
+++ SceneKitTest/SceneKitTest/PlantView.swift
@@ -6,99 +6,49 @@
 //
 
 import SwiftUI
+import SceneKit
 
 struct PlantView: View {
-    @State var moveView = false
-    let plants: [Plants] = [
-        Plants(
-            chapterNum: 1,
-            missionIndex: 1,
-            plantInfos: [
-                PlantInfos(index: 1, fileName: "Tree_small_bare"),
-                PlantInfos(index: 2, fileName: "Tree_small_regular")
-            ]
-        ),
-        Plants(
-            chapterNum: 1,
-            missionIndex: 2,
-            plantInfos: [
-                PlantInfos(index: 1, fileName: "Tree_small_regular"),
-                PlantInfos(index: 2, fileName: "Tree_small_bare")
-            ]
-        )
-    ]
-    
-    @State var forest = [Forest]()
-    
     var body: some View {
-        NavigationView() {
-            VStack {
-                Button(action: {
-                    moveView = true
-                }, label: {
-                    NavigationLink(destination: ForestView(forest: $forest),
-                                   isActive: $moveView,
-                                   label: {Text("숲으로")}
-                    )
-                })
-                
-                TabView {
-                    Plant(
-                        plantInfo: plants.filter{$0.missionIndex == 1}.first!.plantInfos,
-                        forset: $forest
-                    )
-                    .tabItem {
-                        Text("First")
-                    }
-    
-                    Plant(
-                        plantInfo: plants.filter{$0.missionIndex == 2}.first!.plantInfos,
-                        forset: $forest
-                    )
-                    .tabItem {
-                        Text("Second")
-                    }
-                }
-            }
+        ZStack {
+            Image("STR_Img_bg_1_spring")
+                .resizable()
+                .ignoresSafeArea()
+            //MARK: 3d file name 변경 (dae file editor에서 .scn으로 변환 후 사용)
+            PlantSceneView(sceneViewName: .constant("dandelion_1.scn"))
+                .frame(width: 300, height: 500)
         }
     }
 }
 
-struct Plant: View {
-    var plantInfo: [PlantInfos]
-    @State var plantIndex: Int = 0
-    @State var plantName: String = ""
-    @Binding var forset: [Forest]
+struct PlantSceneView: UIViewRepresentable {
+    let sceneView = SCNView()
+    @Binding var sceneViewName: String
     
-    var body: some View {
-        VStack {
-            SceneViewWrapper(
-                updateObject: $plantName,
-                forest: .constant([]),
-                showModal: .constant(false),
-                selectedName: .constant("")
-            )
-            .edgesIgnoringSafeArea(.all)
-            
-            Button(action: {
-                if plantIndex < 2 {
-                    plantIndex += 1
-                    plantName = plantInfo.filter{$0.index == plantIndex}.first!.fileName
-                    
-                    if plantIndex == 2 {
-                        if plantName == "Tree_small_bare" {
-                            forset.append(Forest(fileName: plantName, positionX: 0.2, positionY: 0, positionZ: 0))
-                        }else {
-                            forset.append(Forest(fileName: plantName, positionX: 0, positionY: 3, positionZ: 0))
-                        }
-                    }
-                }
-            }, label: {
-                Text("Update Plant")
-            })
-        }
-        .onAppear() {
-            plantName = "Grass"
+    func makeUIView(context: Context) -> SCNView {
+        sceneView.backgroundColor = .clear
+        sceneView.scene = SCNScene(named: sceneViewName)
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.allowsCameraControl = true
+        //sceneView.defaultCameraController.maximumVerticalAngle = 0.001
+        
+        return sceneView
+    }
+    
+    func updateUIView(_ scnView: SCNView, context: Context) {
+        scnView.scene = SCNScene(named: sceneViewName)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    
+    class Coordinator: NSObject {
+        let parent: PlantSceneView
+        
+        init(_ parent: PlantSceneView) {
+            self.parent = parent
         }
         
     }
